393945da0f660ec9ac6e85ed2df5e485385cc6a2
Moritz Müller <moritz.mueller@salesforce.com>
2018-12-17 11:20:12 +0100

    Added default sorting rules #13
    Added default sorting rules from SiteGenesis; set root sorting to
    best-match

--- src/main/java/com/salesforce/comdagen/model/SortingRule.kt
+++ src/main/java/com/salesforce/comdagen/model/SortingRule.kt
@@ -15,6 +15,40 @@ data class SortingRule(private val seed: Long) {
 
     val description: String
         get() = RandomData.getRandomSentence(seed + "ruleDescription".hashCode())
+
+    /**
+     * A number of default sorting rules, that are hardcoded in the freemarker template.
+     */
+    enum class DefaultRule(val id: String, val description: String) {
+        EINSTEIN(
+            "Einstein Automatic Sort for Revenue", "Sort by Einstein determined" +
+                    " attributes to optimize for revenue"
+        ),
+        BEST_MATCH(
+            "best-matches", "Applies static" +
+                    " sortings (category position, search placement/rank), then text relevance, then" +
+                    "            explicit sortings"
+        ),
+        BRAND("brand", "Sorts by product brand A-Z"),
+        CUSTOMER_FAVORITE("customer-favorites", "Sorts by customer ratings"),
+        MOST_POPULAR(
+            "most-popular",
+            "Sorts by combination of product views, sales velocity, look to book, and availability"
+        ),
+        PRICE_HIGHLOW("price-high-to-low", "Sorts by price descending"),
+        PRICE_LOWHIGH("price-low-to-high", "Sorts by price ascending"),
+        PRODUCTNAME_ASC("product-name-ascending", "Sorts by product name A-Z"),
+        PRODUCTNAME_DESC("product-name-descending", "Sorts by product name Z-A"),
+        TOPSELLER(
+            "top-sellers", "Sorts by combination of revenue, units," +
+                    " look to book, and availability"
+        );
+
+        override fun toString(): String {
+            return id
+        }
+    }
+
 }
 
 data class SortingRuleAssignment(val categoryId: String, val ruleId: String)
